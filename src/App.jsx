8f33e2c1e1e3449835f169ff373c6136b60185e7
Chris DeMars <cdemars86@gmail.com>
2023-11-02 12:48:51 -0400

    adds harness feature flag react sdk and ff code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Logo from './components/Logo/Logo';
 import PrimaryNav from './components/PrimaryNav/PrimaryNav';
 import './index.scss'
+import { FFContextProvider } from '@harnessio/ff-react-client-sdk'
 
 const App = () => {
   return (
+    <FFContextProvider apiKey='b60de57f-159e-4439-bef6-d9c90d32d97a' target={{name:'name', identifier: 'id'}}>
     <div className='navbar-wrapper'>
       <h1 className='title'>Trunk Based Development with Feature Flags</h1>
       <header className='header'>
@@ -11,6 +13,7 @@ const App = () => {
         <PrimaryNav />
       </header>
     </div>
+    </FFContextProvider>
   );
 };
 
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,4 +1,16 @@
+import { useFeatureFlag, useFeatureFlags } from "@harnessio/ff-react-client-sdk";
+
 const Logo = () => {
+  const isEnabled  = useFeatureFlag("awesomelogoheadergradient");
+  const FeatureFlags = useFeatureFlags();
+  console.log(FeatureFlags);
+  if (isEnabled) {
+    return (
+      <h1 className="logo-title gradient">
+        YourHOLLA
+      </h1>
+    );
+  }
   return <h1 className='logo-title'>YourLogo</h1>;
 };
 
