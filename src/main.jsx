f8abd022a502f3917057538e46ecb9e7ba683676
Chris DeMars <cdemars86@gmail.com>
2023-11-01 22:23:13 -0400

    stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  FFContextProvider,
+  useFeatureFlag,
+  useFeatureFlags
+} from '@harnessio/ff-react-client-sdk'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
