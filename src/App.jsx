e74c46d330ffaa1408176b3c7729dafe95f139df
Chris DeMars <cdemars86@gmail.com>
2023-11-06 14:58:10 -0500

    removes useFeatureFlags

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { FFContextProvider } from '@harnessio/ff-react-client-sdk'
 
 const App = () => {
   return (
-    <FFContextProvider apiKey='b60de57f-159e-4439-bef6-d9c90d32d97a' target={{name:'name', identifier: 'id'}}>
+    <FFContextProvider 
+      apiKey='b60de57f-159e-4439-bef6-d9c90d32d97a' 
+      target={{name:'name', identifier: 'id'}}>
     <div className='navbar-wrapper'>
       <h1 className='title'>Trunk Based Development with Feature Flags</h1>
       <header className='header'>
